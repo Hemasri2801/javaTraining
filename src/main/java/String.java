b411d0bfb53141615868f28cf9bda4fa6cd00781
h.venkatachalapathy <hema.venkat128@gmail.com>
2026-01-02 15:46:06 +0530

    string program

--- src/main/java/String.java
+++ src/main/java/String.java
@@ -6,7 +6,7 @@ public class String {
 
         public static void main(java.lang.String[] args) {
             Scanner sc=new Scanner(System.in);
-            System.out.println("Enter a string:");
+            System.out.println("Enter string:");
             java.lang.String st= sc.nextLine();
             java.lang.String rev = "";
             for (int i = st.length() - 1; i >= 0; i--) {
