20f8345986d4f43feb6e5e44e2714ff3cda845cd
h.venkatachalapathy <hema.venkat128@gmail.com>
2026-01-02 15:57:41 +0530

    Inheritance program

---
+++ src/main/java/Inheritance.java
@@ -0,0 +1,28 @@
+package src.main.java;
+
+import java.lang.String;
+
+public class Inheritance {
+        int legs = 4;
+        public void eat() {
+            System.out.println("animal can eat");
+        }
+        public void walk() {
+            System.out.println("animal can walk");
+        }}
+
+    class dog extends Inheritance {
+        void bark() {
+            System.out.println("dog can bark");
+        }
+    }
+    class call {
+        public static void main(String[] args) {
+            dog obj=new dog();
+            System.out.println(obj.legs);
+            obj.bark();
+            obj.eat();
+            obj.walk();}
+    }
+
+
