96b12f7fbe7def978a3c7c69e4bb3fc54a3edd55
h.venkatachalapathy <hema.venkat128@gmail.com>
2026-01-06 17:22:27 +0530

    Constructor program

---
+++ src/main/java/ConstructorExample.java
@@ -0,0 +1,19 @@
+public class ConstructorExample {
+    String name;
+    int age;
+    public ConstructorExample(String n, int a) {
+        name = n;
+        age = a;
+    }
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+    public static void main(String[] args) {
+        ConstructorExample s1 = new ConstructorExample("Hema", 15);
+        ConstructorExample s2 = new ConstructorExample("Iniya", 16);
+        s1.display();
+        s2.display();
+    }
+}
+
